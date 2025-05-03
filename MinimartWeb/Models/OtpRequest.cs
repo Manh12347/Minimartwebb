ff656de1ba7ec5cf76952c38f88f4d2be31573f6
William Christopher <nyaispiano257@gmail.com>
2025-05-03 10:14:57 +0700

    a

--- MinimartWeb/Models/OtpRequest.cs
+++ MinimartWeb/Models/OtpRequest.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MinimartWeb.Model
 {
-    public class OtpRequest
+    public class OtpRequest 
     {
         [Key]
         public int OtpRequestID { get; set; }
